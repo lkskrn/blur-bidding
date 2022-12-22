28f49bd3c7f0c7a48751e6a35ffc6bdf0eb01945
lkskrn <oleksanderkorn@gmail.com>
2022-12-22 19:19:03 +0100

    Add method to withdraw from pool.

--- blur-pool.py
+++ blur-pool.py
@@ -4,6 +4,9 @@ from termcolor import cprint
 from web3 import Web3
 
 
+poolAmount = 0.01
+
+
 def blurAddToPool(privatekey):
 
     def add_to_pool():
@@ -21,7 +24,7 @@ def blurAddToPool(privatekey):
             nonce = web3.eth.get_transaction_count(address_wallet)
 
             contract_txn = contract.functions.deposit().buildTransaction({
-                'value': web3.toWei(0.01, 'ether'),
+                'value': web3.toWei(poolAmount, 'ether'),
                 'nonce': nonce,
             })
 
@@ -34,7 +37,38 @@ def blurAddToPool(privatekey):
         except Exception as error:
             cprint(f'\n>>> {address_wallet} {error}', 'red')
 
-    add_to_pool()
+    def withdraw_from_pool():
+        try:
+            RPC = "https://eth-mainnet.g.alchemy.com/v2/ujAP2FT6E7-oJWdWuSRaRNma4iXcdNhy"
+            web3 = Web3(Web3.HTTPProvider(RPC))
+            account = web3.eth.account.privateKeyToAccount(privatekey)
+            address_wallet = account.address
+            contractToken = Web3.toChecksumAddress(
+                '0x0000000000A39bb272e79075ade125fd351887Ac')
+            ABI = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"previousAdmin","type":"address"},{"indexed":false,"internalType":"address","name":"newAdmin","type":"address"}],"name":"AdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"beacon","type":"address"}],"name":"BeaconUpgraded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint8","name":"version","type":"uint8"}],"name":"Initialized","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"implementation","type":"address"}],"name":"Upgraded","type":"event"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"deposit","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"proxiableUUID","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"}],"name":"upgradeTo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"upgradeToAndCall","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]'
+            contract = web3.eth.contract(address=contractToken, abi=ABI)
+
+            nonce = web3.eth.get_transaction_count(address_wallet)
+            amount = web3.toWei(poolAmount, 'ether')
+            gasLimit = web3.eth.estimate_gas({'to': Web3.toChecksumAddress(address_wallet), 'from': Web3.toChecksumAddress(
+                address_wallet), 'value': web3.toWei(0.001, 'ether')}) + random.randint(50000, 100000)
+            contract_txn = contract.functions.withdraw(amount).buildTransaction({
+                'nonce': nonce,
+                "gas": gasLimit,
+                'gasPrice': web3.eth.gas_price,
+            })
+
+            signed_txn = web3.eth.account.sign_transaction(
+                contract_txn, private_key=privatekey)
+            tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+            cprint(
+                f'\n>>> https://etherscan.io/tx/{web3.toHex(tx_hash)}', 'green')
+        except Exception as error:
+            cprint(f'\n>>> {address_wallet} {error}', 'red')
+
+    add_to_pool()  # Uncomment to add to pool
+    # withdraw_from_pool()  # Uncomment to withdraw from pool
     time.sleep(random.randint(2, 4))
 
 
